432727a3d65e00955d199fa3de6211f6f6732765
James Mernin <jamespmernin@gmail.com>
2020-09-16 11:01:48 -0400

    Cleaned up logic

--- color-scheme-switcher/js/main.js
+++ color-scheme-switcher/js/main.js
@@ -1,9 +1,9 @@
 // First, select each button.
-let buttons = document.querySelectorAll('.js-buttons li');
+let buttons = document.querySelectorAll('a');
 
 // Second, write an event listener for each of the buttons you selected. You can use callback functions or anonymous functions.
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', () => {
-    document.querySelector('body').className = buttons[i].querySelector('a').className;
+buttons.forEach((circle) => {
+  circle.addEventListener('click', () => {
+    document.body.className = circle.className;
   })
-}
+})
